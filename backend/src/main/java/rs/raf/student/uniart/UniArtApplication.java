1ad6b11836a222ffb545a1df4dcd8bd5244025b5
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-09-12 00:30:10 +0200

    Backend | initialise project

---
+++ backend/src/main/java/rs/raf/student/uniart/UniArtApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.student.uniart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniArtApplication.class, args);
+	}
+
+}
