c6dafd6a65133ffddd4c348d4360e24f7bf21806
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-09-12 04:14:36 +0200

    Backend | implement Editor data transfer objects

---
+++ backend/src/main/java/rs/raf/student/uniart/dto/user/editor/EditorGetDto.java
@@ -0,0 +1,30 @@
+package rs.raf.student.uniart.dto.user.editor;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import rs.raf.student.uniart.dto.user_role.UserRoleGetDto;
+
+import java.time.LocalDate;
+
+@Data
+public class EditorGetDto {
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String username;
+
+    private String email;
+
+    @JsonProperty("date_of_birth")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate dateOfBirth;
+
+    @JsonProperty("user_role")
+    private UserRoleGetDto userRole;
+
+}
